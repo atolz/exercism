d7ee04e1ee2ad3238c33945575d4c8d93943adf6
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-04-04 20:10:39 +0100

    track-position-mobile:px-to-rem

--- components/TrackMenu.js
+++ components/TrackMenu.js
@@ -14,11 +14,11 @@ const TrackMenu = ({ show, tracks, setSelectedValue, selectedValue, track_counts
   // });
   return (
     <div
-      className={` h-[26.25rem] w-full max-w-[376px] scroll_hide overflow-y-scroll overflow-x-hidden bg-white absolute top-20  left-7 p-2 rounded-lg exe-shadow z-30 transition-all ${
+      className={` h-[26.25rem] w-full max-w-[23.5rem] scroll_hide overflow-y-scroll overflow-x-hidden bg-white absolute top-20 left-5 md:left-7 p-2 rounded-lg exe-shadow z-30 transition-all ${
         show ? " visible opacity-100" : "opacity-0 invisible"
       }`}
     >
-      <ul className="max-w-[376px] w-full">
+      <ul className="max-w-[23.5rem] w-full">
         {/* All tracks */}
         <li
           onClick={() => {
@@ -29,8 +29,8 @@ const TrackMenu = ({ show, tracks, setSelectedValue, selectedValue, track_counts
           <MyRadio ariaLabel={"All"} isChecked={selectedValue === "All"} onChange={handleChange} id={"All"} name="radio-buttons"></MyRadio>
 
           {/* <Radio checked={selectedValue === "All"} onChange={handleChange} value="All" name="radio-buttons" inputProps={{ "aria-label": "All" }} /> */}
-          <img className="ml-[26px] w-[2.35rem]" src="/icons-svg/exe-badge.svg" />
-          <span className=" ml-[18.23px] font-medium text-base leading-[2.2rem] text-[#3D3B45] mr-auto">All</span>
+          <img className="ml-[1.625rem] w-[2.35rem]" src="/icons-svg/exe-badge.svg" />
+          <span className=" ml-[1.139rem] font-medium text-base leading-[2.2rem] text-[#3D3B45] mr-auto">All</span>
           <span className=" flex">
             <Count color={`${selectedValue === "All" ? "#130B43" : ""}`} borderColor={`${selectedValue === "All" ? "#6A6781" : ""}`} value={totalTracks}></Count>
           </span>
@@ -51,8 +51,8 @@ const TrackMenu = ({ show, tracks, setSelectedValue, selectedValue, track_counts
             >
               <MyRadio ariaLabel={track.titles} isChecked={selectedValue === track.slug} onChange={handleChange} id={track.slug} name="radio-buttons"></MyRadio>
               {/* <Radio checked={selectedValue === track.slug} onChange={handleChange} value={track.slug} name="radio-buttons" inputProps={{ "aria-label": track.title }} /> */}
-              <img className="ml-[26px] ml-6 w-[2.35rem]" src={track.icon_url} />
-              <span className=" ml-[18.23px] font-medium text-base leading-[2.2rem] text-[#3D3B45] mr-auto">{track.title}</span>
+              <img className="ml-[1.625rem] ml-6 w-[2.35rem]" src={track.icon_url} />
+              <span className=" ml-[1.139rem] font-medium text-base leading-[2.2rem] text-[#3D3B45] mr-auto">{track.title}</span>
               <span className="flex">
                 <Count color={`${selectedValue === track.slug ? "#130B43" : ""}`} borderColor={`${selectedValue === track.slug ? "#6A6781" : ""}`} value={track_counts[track.slug]}></Count>
               </span>
